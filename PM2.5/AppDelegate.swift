e329b99246621fbea39927f892db8fac110c2e8d
DMCL <dmcl@DMCLdeMac-mini.local>
2017-07-01 15:57:54 +0800

    first

--- PM2.5/AppDelegate.swift
+++ PM2.5/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().barTintColor = UIColor(red: 220/255 ,green: 110/255 , blue: 67/255 ,alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white]
+        
+        IQKeyboardManager.sharedManager().enable = true
+        
+        
         return true
     }
 
